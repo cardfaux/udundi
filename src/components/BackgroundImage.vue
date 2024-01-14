3d186a0e169f34bfd832ead43a69c4433b7387cf
James Hagood <cardfaux@tutanota.com>
2024-01-14 17:41:21 -0600

    preloaded the image

--- src/components/BackgroundImage.vue
+++ src/components/BackgroundImage.vue
@@ -1,11 +1,21 @@
 <script setup></script>
 
 <template>
-  <div class="udundi h-screen bg-cover bg-no-repeat bg-[url('/img/Coding-Challenge-Image.png')]"></div>
+  <!-- <div class="udundi h-screen bg-cover bg-no-repeat bg-[url('/img/Coding-Challenge-Image.png')]">
+    <img src="/img/Coding-Challenge-Image.png" alt="forest background image" rel="preload" />
+  </div> -->
+  <div class="udundi h-screen bg-cover bg-no-repeat">
+    <img src="/img/Coding-Challenge-Image.png" alt="forest background image" rel="preload" />
+  </div>
 </template>
 
 <style scoped>
 .udundi {
   background-color: #611818;
+}
+.udundi img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 </style>
